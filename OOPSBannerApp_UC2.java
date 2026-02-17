3305830b62da665e665700b4e12c02aa7b327c6c
it-me-kkaran11 <rkarunakaran174@gmail.com>
2026-02-17 13:12:57 +0530

    Add files via upload

---
+++ OOPSBannerApp_UC2.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+/**
+ * OOPSBannerApp - UC2
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Karunakaran
+ * @version 1.0
+ */
+
+public class OOPSBannerApp_UC2 {
+
+    public static void main(String[] args) {
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" ***     ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"***      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**   **  "+" "+"   ***   ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"      ***");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"     *** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
+    }
+}
