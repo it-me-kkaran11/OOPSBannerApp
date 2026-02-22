76ca2e070c9a7f54887f03bfd79fce82bcec6260
it-me-kkaran11 <rkarunakaran174@gmail.com>
2026-02-22 21:28:39 +0530

    UC2 implemented

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 /**
- * OOPSBannerApp
- *
+ * OOPSBannerApp is a simple Java application that prints a banner with the text "OOPS" to the console.
  * Prints the literal text "OOPS" to the console.
  *
- * @author Karunakaran R
+ * @author Karunakaran
  * @version 1.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" ***     ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"***      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**   **  "+" "+"   ***   ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"      ***");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"     *** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
     }
 }
